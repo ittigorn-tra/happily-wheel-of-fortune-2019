f31ff31008bf8a3b4b08c4eabe76462099f3efe5
Ittigorn Tradussadee <ittigorn_t@dhas.com>
2019-05-24 17:27:19 +0700

    Still adjusting degrees

--- prizes.lua
+++ prizes.lua
@@ -1,9 +1,11 @@
 prizes = {
-  paperback = { name='สมุดปกอ่อน', sprite = nil },
+  softcover = { name='สมุดปกอ่อน', sprite = nil },
   hardcover = { name='สมุดปกแข็ง', sprite = nil },
-  sketch1   = { name='Sketchbook Set 1', sprite = nil },
-  sketch2   = { name='Sketchbook Set 2', sprite = nil },
+  sketch1   = { name='Sketchbook', sprite = nil },
+  sketch2   = { name='Sketchbook Large', sprite = nil },
+  sketchSet1   = { name='Sketchbook Rainbow Set', sprite = nil },
+  sketchSet2   = { name='Sketchbook Summer Set', sprite = nil },
   pen       = { name='ปากกา', sprite = nil },
-  shirtW    = { name='Happily T-shirt White', sprite = nil },
-  shirtB    = { name='Happily T-shirt Black', sprite = nil },
+  shirt    = { name='Happily T-shirt Black / White', sprite = nil },
+  blank    = { name='Blank', sprite = nil },
 }
